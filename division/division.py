9f1cd39d1b04480eec59f2816abf6d0cb2474981
Irene Shi <irene.shi_jy@berkeley.edu>
2023-03-03 18:54:40 +0000

    create division directory

---
+++ division/division.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+def division(numerator, denominator):
+    if denominator == np.isclose(denominator, 0.0):
+        raise ZeroDivisionError("You cannot divide by denominator={}".format(denominator))
+    return numerator / denominator
+
